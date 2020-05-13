0e9c806e5249c378d187ab3d0d1b3093593193e6
Moxiney <Moxiney@users.noreply.github.com>
2020-05-13 15:27:44 +0800

    test benchmark_common.h availability

--- src/main/sample/sample.cpp
+++ src/main/sample/sample.cpp
@@ -2,10 +2,13 @@
 
 #include "storage/data_table.h"
 #include "benchmark/benchmark_sample.h"
+#include "benchmark/benchmark_common.h"
 
 int main() {
     std::cout<<"Hello World"<<std::endl;
     Foo();
     Hoo();
+    peloton::benchmark::FastRandom fastRand(0);
+    std::cout<<fastRand.next_u32()<<std::endl;
     return 0;
 }
