7cdda905ed340f6e80088509083dbd92ef20d334
Moxiney <Moxiney@users.noreply.github.com>
2020-05-13 17:28:57 +0800

    test logger

--- src/main/sample/sample.cpp
+++ src/main/sample/sample.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 
-#include "storage/data_table.h"
+
 #include "benchmark/benchmark_sample.h"
 #include "benchmark/benchmark_common.h"
+
+#include "storage/data_table.h"
+#include "common/logger.h"
 
 int main() {
     std::cout<<"Hello World"<<std::endl;
@@ -10,5 +13,7 @@ int main() {
     Hoo();
     peloton::benchmark::FastRandom fastRand(0);
     std::cout<<fastRand.next_u32()<<std::endl;
+    
+    peloton::LOG_INFO("Hello World");
     return 0;
 }
